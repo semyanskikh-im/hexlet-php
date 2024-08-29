37622edeab1aa79cb4552439fc802774747a670e
semyanskikh-im <semyanskikh@gmail.com>
2024-08-29 13:02:11 +0300

    add Webmozart\Assert

--- tests/StringUtilsTest.php
+++ tests/StringUtilsTest.php
@@ -1,13 +1,15 @@
 <?php
 
-require("src/StringUtils.php");
+require_once("vendor/autoload.php");
 
-if (StringUtils\capitalize('hello') !== 'Hello') {
-    throw new \Exception('Функция работает неверно!');
-}
+use Webmozart\Assert\Assert;
+use function StringUtils\capitalize;
 
-if (StringUtils\capitalize('') !== '') {
-    throw new \Exception('Функция работает неверно!');
-}
+// В строке ниже eq означает equal
+Assert::eq(capitalize(''), '');
+// Первый параметр actual – то, что пришло
+// Второй параметр expected – то, что ожидает тест
+// Правильный порядок аргументов имеет большое значение при анализе ошибки
+Assert::eq(capitalize('hello'), 'Hello');
 
 echo "Все тесты пройдены!\n";
